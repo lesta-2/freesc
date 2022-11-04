6d272e790e75855ebb86e6f19d3d03e51a8038a6
lesta-2 <39772702+lesta-2@users.noreply.github.com>
2022-11-04 07:33:09 -0700

    Create install-xray.sh

---
+++ install-xray.sh
@@ -0,0 +1,27 @@
+cd /usr/bin
+wget -O add-ws "https://raw.githubusercontent.com/lesta-2/freesc/main/add-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/lesta-2/freesc/main/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/lesta-2/freesc/main/add-tr.sh"
+wget -O del-ws "https://raw.githubusercontent.com/lesta-2/freesc/main/del-ws.sh"
+wget -O del-vless "https://raw.githubusercontent.com/lesta-2/freesc/main/del-vless.sh"
+wget -O del-tr "https://raw.githubusercontent.com/lesta-2/freesc/main/del-tr.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/lesta-2/freesc/main/cek-ws.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/lesta-2/freesc/main/cek-vless.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/lesta-2/freesc/main/cek-tr.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/lesta-2/freesc/main/renew-ws.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/lesta-2/freesc/main/renew-vless.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/lesta-2/freesc/main/renew-tr.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/lesta-2/freesc/main/cert.sh"
+chmod +x add-ws
+chmod +x add-vless
+chmod +x add-tr
+chmod +x del-ws
+chmod +x del-vless
+chmod +x del-tr
+chmod +x cek-ws
+chmod +x cek-vless
+chmod +x cek-tr
+chmod +x renew-ws
+cmod +x renew-vless
+chmod +x renew-tr
+chmod +x certv2ray
